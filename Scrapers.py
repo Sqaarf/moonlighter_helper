04e9e8566441703fac35747921a0b63407484c8e
Sqaarf <mrscarf19@gmail.com>
2022-12-22 21:22:57 +0100

    Added initItemList function to improve readability

--- Scrapers.py
+++ Scrapers.py
@@ -5,9 +5,11 @@ class ItemScraper:
     def __init__(self) -> None:
         self.page = requests.get("https://steamcommunity.com/sharedfiles/filedetails/?id=1757126124")
         self.soup = bs(self.page.text, "html.parser")
-
+        self.item_list = self.initItemList()
+        
+    
+    def initItemList(self) -> list:
         tr_tags = self.soup.find_all("div", attrs={"class": "bb_table_tr"})[:242]
-
         td_tags = []
 
         for tr_tag in tr_tags:
@@ -15,18 +17,20 @@ class ItemScraper:
 
         keys_list = [key.text for key in td_tags[0]]
 
-        self.item_list = []
+        ret = []
         index = 0
 
         for entry in td_tags:
             if entry[0].text == "Name":
                     continue
             else:
-                self.item_list.append({})
+                ret.append({})
                 for i in range(len(entry)):
-                        self.item_list[index][keys_list[i]] = entry[i].text
+                        ret[index][keys_list[i]] = entry[i].text
                 index += 1
-    
+        
+        return ret
+
     def getItem(self, item) -> dict:
         for entry in self.item_list:
             if entry["Name"] == item:
