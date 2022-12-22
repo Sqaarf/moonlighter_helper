fbe4af94db0c619d35ae8ca0dddeda017c5096f3
Sqaarf <mrscarf19@gmail.com>
2022-12-22 22:46:08 +0100

    Added None handling

--- main.py
+++ main.py
@@ -2,12 +2,16 @@ from Scrapers import ItemScraper
 
 if __name__ == "__main__":
     scraper = ItemScraper()
-    
+
     while True:
         item = input("Prompt the item : ")
 
         if item == "exit" or item == "":
             break
+
+        if scraper.getItem(item) == None:
+            print("Error : No item existing in this name")
+            continue
 
         name = scraper.getItem(item)['Name']
         pprice = scraper.getItem(item)['Perfect Price (Popular)']
